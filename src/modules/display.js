0629d7c6a7c2ff2327ff7d4f4e10752680883006
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-03 18:28:55 -0300

    Made recommended changes

--- src/modules/display.js
+++ src/modules/display.js
@@ -12,21 +12,17 @@ const display = () => {
     arr = [];
   }
   arr.forEach((item) => {
-    if (item.completed === true) {
-      tasklist.innerHTML += `<div class="field" id=${item.index}>
+      tasklist.innerHTML += item.completed === true ? `<div class="field wide" id=${item.index}>
     <div class="inputcontainer">
       <label for=${item.index}><input type="checkbox" class="task" checked><div class="tasktext ${item.completed}">${item.description}</div></label>
     </div>
     <button class="listbutton" id=button${item.index} type="button" title="Change Task"><ion-icon name="ellipsis-vertical"></ion-icon></button>
-  </div>`;
-    } else {
-      tasklist.innerHTML += `<div class="field" id=${item.index}>
+  </div>` : `<div class="field" id=${item.index}>
     <div class="inputcontainer">
       <label for=${item.index}><input type="checkbox" class="task"><div class="tasktext ${item.completed}">${item.description}</div></label>
     </div>
     <button class="listbutton" id=button${item.index} type="button" title="Change Task"><ion-icon name="ellipsis-vertical"></ion-icon></button>
   </div>`;
-    }
   });
   complete();
   targeter();
