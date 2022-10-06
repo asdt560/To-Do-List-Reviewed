7f5a9028449eff63b6034772f339e81cf78c4d05
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-06 12:35:24 -0300

    Add display test

--- test/add-remove.test.js
+++ test/add-remove.test.js
@@ -2,21 +2,39 @@
  * @jest-environment jsdom
  */
 
-import { list } from '../src/modules/class.js';
+import { TaskList } from '../src/modules/class.js';
+
+import display from '../src/modules/display.js';
+
+const list = new TaskList();
 
 describe('add and remove tests', () => {
-  beforeEach(() => {
-    window.localStorage.clear();
-  });
   it('add test', () => {
     list.add('example');
+    list.add('example1');
+    const example = JSON.parse(localStorage.getItem('tasklist'));
+    expect(example.length).toEqual(2);
+  });
+  it('remove test', () => {
+    list.remove(0);
     const example = JSON.parse(localStorage.getItem('tasklist'));
     expect(example).toEqual([
       {
-        description: 'example',
+        description: 'example1',
         completed: false,
         index: 0,
       },
     ]);
+  });
+  it('display test', () => {
+    document.body.innerHTML = '<div id="dynamiccontainer" class="wide"></div>';
+    const tasklist = document.querySelector('#dynamiccontainer');
+    display();
+    expect(tasklist.innerHTML).toEqual(`<div class="field" id="0">
+    <div class="inputcontainer">
+      <label for="0"><input type="checkbox" class="task"><div class="tasktext false">example1</div></label>
+    </div>
+    <button class="listbutton" id="button0" type="button" title="Change Task"><ion-icon name="ellipsis-vertical"></ion-icon></button>
+  </div>`);
   });
 });
