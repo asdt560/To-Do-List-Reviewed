8a9449034507905392f37d97add1c1e5282db0cd
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-06 10:21:40 -0300

    Add first successful test

--- test/add-remove.test.js
+++ test/add-remove.test.js
@@ -1,7 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { list } from '../src/modules/class.js';
 
 describe('add and remove tests', () => {
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
   it('add test', () => {
-    expect(list.add('example'))
+    list.add('example');
+    const example = JSON.parse(localStorage.getItem('tasklist'));
+    expect(example).toEqual([
+      {
+        description: 'example',
+        completed: false,
+        index: 0,
+      },
+    ]);
   });
 });
